b201a75e0f71cfb7d65de24070d933925798a54b
Oscar Treviño <44791985+oscartrevino-ti@users.noreply.github.com>
2023-10-28 12:42:31 -0600

    Update 8 main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const formulario = document.querySelector('#formulario');
 const buttonSubmit = document.querySelector('#submit');
 const urlDesktop = 'https://web.whatsapp.com/';
 const urlMobile = 'whatsapp://';
-const telefono = '528133873447';
+const telefono = '+528133873447';
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault()
